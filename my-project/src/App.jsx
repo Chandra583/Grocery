1e493cbd068bf7cb499075b081e500306d0d75b8
Chandra583 <chandrashekhargawda2000@gmail.com.com>
2023-07-31 12:53:02 +0530

    starting package of tailwind

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -28,8 +28,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
+      
     </>
   )
 }
-
 export default App
