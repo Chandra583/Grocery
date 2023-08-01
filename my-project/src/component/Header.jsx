f40556eb14b358fa22cad4213a552c20c9127c16
Chandra583 <chandrashekhargawda2000@gmail.com.com>
2023-08-01 15:51:16 +0530

    nothing changed

--- my-project/src/component/Header.jsx
+++ my-project/src/component/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
               </div> */}
             {/* </Link> */}
           </div>
-          <div className="text-2xl text-slate-600 relative">
+          <div className="text-2xl text-slate-500 relative">
             <FaUserAlt/>
           </div>
         </div>
