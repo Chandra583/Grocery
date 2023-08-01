5332d13127f3e7429ab3076b45bbf8942c24ef4d
Chandra583 <chandrashekhargawda2000@gmail.com.com>
2023-08-01 15:48:47 +0530

    nav bar initial done

--- my-project/src/component/Header.jsx
+++ my-project/src/component/Header.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assest/logo.png";
+// import {BiSolidUser} from "react-icons/fa"
+import { FaUserAlt } from 'react-icons/fa'
+import { BsCartFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <header>
       {/* Destop version */}
       <div className="flex items-center h-full justify-between">
-      <div className="h-10">
-        <img src={logo} className="h-full" />
+        <Link to={""}>
+          <div className="h-10">
+            <img src={logo} className="h-full" />
+          </div>
+        </Link>
+
+
+        <div className="flex items-center gap-4 md:gap-7">
+        <nav className="gap-4 md:gap-6 text-base md:text-lg hidden md:flex">
+            <Link to={""}>Home</Link>
+            <Link to={"menu/63f0fdbb3bcc2f97fa53d25d"}>Menu</Link>
+            <Link to={"about"}>About</Link>
+            <Link to={"contact"}>Contact</Link>
+          </nav>
+          <div className="text-2xl text-slate-600 relative">
+            {/* <Link to={"cart"}> */}
+              <BsCartFill />
+              {/* <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center ">
+                {cartItemNumber.length}
+              </div> */}
+            {/* </Link> */}
+          </div>
+          <div className="text-2xl text-slate-600 relative">
+            <FaUserAlt/>
+          </div>
         </div>
       </div>
 
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -4,8 +4,24 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route index element={<Home />} />
+      <Route path="menu" element={<Menu />} />
+      <Route path="menu/:filterby" element={<Menu />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+      <Route path="newproduct" element={<Newproduct />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="success" element={<Success/>}/>
+      <Route path="cancel" element={<Cancel/>}/> */}
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
